7dda38975eeebafb7bbfbe19045e748f27b2e455
Lara Samara <136354923+LaraSamara@users.noreply.github.com>
2024-01-23 23:02:49 +0200

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,13 +1,6 @@
-import mongoose from 'mongoose';
-
-const connectDB = async ()=>{
-
-    return await mongoose.connect(process.env.DB_LOCAL)
-    .then( ()=>{
-        console.log("connect db");
-    } ).catch( (err)=>{
-        console.log(`error to connect db ${err}`)
-    } )
+import mongoose from "mongoose";
+const connectDB =()=>{
+    return mongoose.connect(process.env.Local_DB);
 }
 
 export default connectDB
