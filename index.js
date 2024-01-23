62d34171b39215c92c2564871827f420d025738d
Lara Samara <136354923+LaraSamara@users.noreply.github.com>
2024-01-23 23:04:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ dotenv.config();
 import express from 'express';
 import initApp from './src/Modules/app.router.js';
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 initApp(app, express);
-app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`);
-});
+app.listen(PORT);
